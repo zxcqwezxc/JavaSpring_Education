637da40f1a0b7c587ad5fe1eec6634c175591b5d
zxcqwezxc <86100830+zxcqwezxc@users.noreply.github.com>
2022-06-26 16:35:49 +0300

    Add files via upload

---
+++ CourseWork/src/main/java/com/training/CourseWork/controllers/VoteController.java
@@ -0,0 +1,4 @@
+package com.training.CourseWork.controllers;
+
+public class VoteController {
+}
---
+++ CourseWork/src/test/java/com/training/CourseWork/CourseWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.CourseWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
